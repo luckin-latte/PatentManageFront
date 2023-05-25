6c2dd14f59ea88065c10c7132e836a8f4485b670
zhangwy <wanyu.zhang@qq.com>
2023-05-25 15:27:21 +0800

    0.0.1

--- src/app/pages/trademark-post-file/trademark-post-file.component.ts
+++ src/app/pages/trademark-post-file/trademark-post-file.component.ts
@@ -1,15 +1,134 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzDrawerRef, NzDrawerService } from 'ng-zorro-antd/drawer';
+
+import { QueryInfo, QueryCriteria, QueryCriteriaInfo } from 'src/app/shared';
 
 @Component({
   selector: 'app-trademark-post-file',
   templateUrl: './trademark-post-file.component.html',
-  styleUrls: ['./trademark-post-file.component.css']
+  styleUrls: ['./trademark-post-file.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TrademarkPostFileComponent implements OnInit {
 
-  constructor() { }
+  public dataSet: any; // 查询列表资料
+  public searchForm: FormGroup;
+  public searchLoading = true;
+  public queryInfo: QueryInfo = new QueryInfo(); // 创建产生查询条件类
+
+  drawerRef!: NzDrawerRef;
+  pageIndex: number = 1;
+  public applyDateRange = [];
+  public empowerDateRange = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private nzDrawerService: NzDrawerService
+  ) {
+    this.searchForm = this.formBuilder.group({});
+    this.searchForm.addControl('code', new FormControl(''));
+    this.searchForm.addControl('name', new FormControl(''));
+    this.searchForm.addControl('type', new FormControl('0'));
+    this.searchForm.addControl('applyCode', new FormControl(''));
+    this.searchForm.addControl('proposer', new FormControl(''));
+  }
 
   ngOnInit(): void {
+    this.search(true);
+  }
+
+  public onChange(result: Date): void {
+    console.log('onChange: ', result);
+  }
+
+  public resetForm(): void {
+    this.searchForm.reset({
+      code: '',
+      name: '',
+      type: '0',
+      applyCode: '',
+      proposer: ''
+    });
+  }
+  
+  // 查询
+  public search(reset: boolean = false): void {
+    this.onBeforeSearch();
+
+    if (reset) {
+      this.queryInfo.pageNum = 1;
+      this.pageIndex = 1;
+    }
+
+    this.queryData();
+
+  }
+  
+  // 获取查询条件
+  public queryData(): void {
+    const queryCriteria = new QueryCriteria(); // 创建查询条件类
+
+    for (const key of Object.keys(this.searchForm.controls)) {
+      if (
+        Array.isArray(this.searchForm.controls[key].value) &&
+        this.searchForm.controls[key].value.length === 0
+      ) {
+        continue;
+      }
+      if (this.searchForm.controls[key].value === '') {
+        continue;
+      }
+
+      if (key === 'code') {
+        queryCriteria.addCriteria(
+          new QueryCriteriaInfo(
+            'code',
+            this.searchForm.controls[key].value
+          )
+        );
+      } else if (key === 'name') {
+        queryCriteria.addCriteria(
+          new QueryCriteriaInfo(
+            'name',
+            this.searchForm.controls[key].value
+          )
+        );
+      } else if (key === 'type') {
+        queryCriteria.addCriteria(
+          new QueryCriteriaInfo(
+            'type',
+            this.searchForm.controls[key].value
+          )
+        );
+      } else if (key === 'applyCode') {
+        queryCriteria.addCriteria(
+          new QueryCriteriaInfo(
+            'applyCode',
+            this.searchForm.controls[key].value
+          )
+        );
+      } else if (key === 'proposer') {
+        queryCriteria.addCriteria(
+          new QueryCriteriaInfo(
+            'proposer',
+            this.searchForm.controls[key].value
+          )
+        );
+      }
+    }
+    
+    this.queryInfo.setCriteria(queryCriteria);
+    console.log('查询条件：',this.queryInfo)
+  }
+  
+
+  onBeforeSearch(): void {
+    this.searchLoading = true;
+  }
+
+  onAfterSearch(): void {
+    this.searchLoading = false;
   }
 
 }
--- src/app/pages/trademark-receive-file/trademark-receive-file.module.ts
+++ src/app/pages/trademark-receive-file/trademark-receive-file.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 
 import { TrademarkReceiveFileRoutingModule } from './trademark-receive-file-routing.module';
 import { TrademarkReceiveFileComponent } from './trademark-receive-file.component';
-
 
 @NgModule({
   declarations: [
@@ -14,8 +24,20 @@ import { TrademarkReceiveFileComponent } from './trademark-receive-file.componen
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     NzPageHeaderModule,
     NzBreadCrumbModule,
+    NzFormModule,
+    NzGridModule,
+    NzInputModule,
+    NzDatePickerModule,
+    NzSelectModule,
+    NzButtonModule,
+    NzIconModule,
+    NzTableModule,
+    NzEmptyModule,
+    NzDrawerModule,
     TrademarkReceiveFileRoutingModule
   ]
 })
